d13a855beb4f9cbffc93aa1dfb0d2b2608fdf5dc
toleZ <toloyjc@gmail.com>
2022-12-08 12:41:08 -0300

    Adding styles to filter/order input

--- src/components/FilterOrder/FilterOrder.jsx
+++ src/components/FilterOrder/FilterOrder.jsx
@@ -1,5 +1,6 @@
 import { useDispatch } from "react-redux";
 import { filterCards, orderCards } from "../../redux/actions";
+import styles from "./FilterOrder.module.css";
 
 const FilterOrder = () => {
   const dispatch = useDispatch();
@@ -8,17 +9,18 @@ const FilterOrder = () => {
     const { name, value } = e.target;
 
     if (name === "Order") dispatch(orderCards(value));
+
     if (name === "Filter") dispatch(filterCards(value));
   };
 
   return (
-    <div>
-      <select name="Order" onChange={handleChange}>
+    <div className={styles.wrapper}>
+      <select name="Order" className={styles.select}>
         <option value="Ascendente">Ascendente</option>
         <option value="Descendente">Descendente</option>
       </select>
 
-      <select name="Filter" onChange={handleChange}>
+      <select name="Filter" onChange={handleChange} className={styles.select}>
         <option value="Male">Male</option>
         <option value="Female">Female</option>
         <option value="Genderless">Genderless</option>
